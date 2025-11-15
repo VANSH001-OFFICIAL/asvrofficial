bc6cf4a2b0c89a8925ebfeced3ab4412e8ef0ba3
VANSH001-OFFICIAL <vansh59rt@gmail.com>
2025-11-15 20:40:30 +0530

    Update wsgi.py

--- website/wsgi.py
+++ website/wsgi.py
@@ -1,9 +1,11 @@
 # File: your_project_name/wsgi.py (Standard Django file)
 
+# ... (lines before)
 import os
+
 from django.core.wsgi import get_wsgi_application
 
-# Ensure ki aapka project name yahaan sahi ho
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings') 
+# Yahaan 'your_project_name' ko 'website' se badlein
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'website.settings')
 
 application = get_wsgi_application()
